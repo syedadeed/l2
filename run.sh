49ca1c72311c3d0d6caef91c1dae2350fcadb31f
Syed Adeed <syedadeed@protonmail.com>
2026-05-10 16:28:35 +0530

    idk

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@
 # docker run --name redis -p 6379:6379 -d redis
 # docker run --name postgres -e POSTGRES_PASSWORD=pg -p 5432:5432 -d postgres
 # docker exec -it postgres psql -U postgres
+# GOEXPERIMENT=jsonv2 go build
