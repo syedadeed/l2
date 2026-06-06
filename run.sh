8da1436a99402100744bd7f006ba2996d656f325
Syed Adeed <syedadeed@protonmail.com>
2026-06-06 20:33:29 +0530

    dk

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # docker run --name redis -p 6379:6379 -d redis
+# docker run -d --name rustfs -p 9000:9000 -p 9001:9001 rustfs/rustfs:latest
 # docker run --name postgres -e POSTGRES_PASSWORD=pg -p 5432:5432 -d postgres
 # docker exec -it postgres psql -U postgres
 # GOEXPERIMENT=jsonv2 go build
